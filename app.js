3d355fc0d6ac0fc6fccf4e3ea860a5c792cef0b8
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-02-04 22:19:17 +0000

    continuing with the gallery slider

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const arrowSlider = document.querySelectorAll(".slider-arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrowSlider.forEach((arrow, index) => {
+  const itemNumber = movieLists[index].querySelectorAll("img").length;
+  let clickCounter = 0;
+  console.log(index, arrow, itemNumber);
+  arrow.addEventListener("click", () => {
+    clickCounter++;
+    // Value 4 is the number of images that are visible at the same time. It is not dynamic in case if you have larger screen.
+    if (itemNumber - (4 + clickCounter) >= 0) {
+      movieLists[index].style.transform = `translateX(${
+        movieLists[index].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[index].style.transform = `translateX(0)`;
+      clickCounter = 0;
+    }
+  });
+});
